4fec6859df638557564c19de2bb5004b52a224ff
PierreCASTAINGv <pcastaing@fabriquenumerique.fr>
2019-12-22 19:48:54 +0100

    version 0.3.0

--- src/component/utils/toggle.js
+++ src/component/utils/toggle.js
@@ -1,8 +1,16 @@
 export function handleToggle(elt) {
-
+        
         const classAccordion = elt.target.parentElement.nextElementSibling;
 
-        if(classAccordion.style.display === '' || classAccordion.style.display === 'none') classAccordion.style.display = 'block'
+        if(classAccordion.style.display === '' || classAccordion.style.display === 'none') { 
 
-        else classAccordion.style.display = 'none'
+                let c = 0;
+                const list = document.querySelectorAll('.accordion');
+                for (c of list) {
+                        c.style.display = 'none';
+                }
+        classAccordion.style.display = 'block';
+        }
+        
+        else classAccordion.style.display = 'none';
 }
